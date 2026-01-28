85ccc598baba158408e3ce8876bca6bafd6ebc78
Duc Quang Nguyen <ducquang.nguyen@gmail.com>
2026-01-28 21:46:21 +0100

    make it leaner

--- src/lib/components/DatawrapperChart.svelte
+++ src/lib/components/DatawrapperChart.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	export let chartId: string;
-
-	let iframe: HTMLIFrameElement;
 </script>
 
-<div class="w-full h-full flex flex-col justify-center items-center">
+<div class="w-full h-full">
 	<iframe
-		bind:this={iframe}
 		title="Datawrapper Chart"
 		src="https://datawrapper.dwcdn.net/{chartId}/"
 		class="w-full h-full border-0"
--- src/lib/components/VizContainer.svelte
+++ src/lib/components/VizContainer.svelte
@@ -19,7 +19,13 @@
 			Custom Component: {step.vizProps.component}
 		</div>
 	{:else if step.vizType === 'image'}
-		<img src={step.vizProps.src} alt="" class="w-full h-full object-cover" />
+		<img
+			src={step.vizProps.src}
+			alt=""
+			class="w-full h-full object-cover"
+			loading="lazy"
+			decoding="async"
+		/>
 	{:else}
 		<div
 			class="w-full h-full bg-theme-bg flex items-center justify-center text-theme-subtext font-light"
